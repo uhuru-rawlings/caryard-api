260bdfe85bce487ceec589e341b32cf730feeb7f
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-03 20:29:30 +0300

    registered models in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 from api.models import Registration,Profiles,CarModels,Messages,CarImages,Cars,Companies,Replies,Lastlogin
 # Register your models here.
+
+admin.site.register(Registration)
+admin.site.register(Profiles)
+admin.site.register(CarModels)
+admin.site.register(Messages)
+admin.site.register(CarImages)
+admin.site.register(Cars)
+admin.site.register(Companies)
+admin.site.register(Replies)
+admin.site.register(Lastlogin)
