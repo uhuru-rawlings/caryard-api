11622b9cfcff7d520b5af9ada2d2bd4fa966843e
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-03 15:18:58 +0300

    added db meta data

--- api/models.py
+++ api/models.py
@@ -8,7 +8,13 @@ class Registration(models.Model):
     password = models.CharField(max_length=300)
     date_added = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = 'Registration'
 
+    def __str__(self):
+        return self.useremail
+        
 class Lastlogin(models.Model):
     user = models.ForeignKey(Registration, CASCADE=models.CASCADE)
     last_login = models.DateTimeField()
