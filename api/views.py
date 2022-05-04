20a951f9ab04e67aa2c491daacb06bf7593e49ec
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-04 14:34:09 +0300

    added hashers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from serializers import registrationSerializer
+from api.models import Registration,Profiles,CarModels,Messages,CarImages,Cars,Companies,Replies,Lastlogin
+from rest_framework.views import api_view
+from django.contrib.auth.hashers import make_password,check_password
 # Create your views here.
