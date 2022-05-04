459cb99f4e2c91f5dce1dfee090aaae08e2865da
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-04 14:54:39 +0300

    added serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from pyexpat import model
 from rest_framework import serializers
 from api.models import Registration,Profiles,CarModels,Messages,CarImages,Cars,Companies,Replies,Lastlogin
@@ -8,3 +9,34 @@ class registrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registration
         fields = ['useremail','username','password']
+
+class profilesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profiles
+        fields = ['user','image','contact','date_added']
+class carmodelsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarModels
+        fields = ['modelname','company','date_added']
+class messageSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Messages
+        fields = ['user','message','date_sent']
+class carimagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarImages
+        fields = ['cardetails','interior','exterior','date_added']
+class carsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Cars
+        fields = ['user','carmodel','years_of_service','Fuelconsumption','description','date_added']
+
+class companiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Companies
+        fields = ['companyname','date_added']
+class repliesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Replies
+        fields = ['user','message','date_sent']
+    
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from serializers import registrationSerializer
+from serializers import registrationSerializer,carmodelsSerializer,messageSerializers,profilesSerializer,carimagesSerializer,carsSerializers,companiesSerializer,repliesSerializers
 from api.models import Registration,Profiles,CarModels,Messages,CarImages,Cars,Companies,Replies,Lastlogin
 from rest_framework.views import api_view
 from django.contrib.auth.hashers import make_password,check_password
